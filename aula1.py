53f42da4ef50f4a519f50918ffdeb039fab72b03
Arthur <arthurmoney21@gmail.com>
2026-02-03 20:54:33 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,74 @@
+# Exemplo 1
+'''
+nome = "Maria"
+idade = 25
+altura = 1.65
+estudante = True
+
+print("nome:", nome)
+print("idade:", idade)
+print("altura:", altura)
+print("É estudante:", estudante)
+'''
+# EX 2
+'''
+preco = 50.00
+quantidade = 3
+total = preco * quantidade 
+
+print("Preço Unitário:", preco)
+print("Quantidade:", quantidade)
+print("Total: R$", total)
+'''
+#Ex 3
+'''
+Nome = input("Qual é o seu nome")
+idade = input("Qual a sua idade ")
+idade = int(idade)
+Soma = idade + 10
+
+print("Sua idade daqui 10 anos será:", Soma)'''
+#ex 4
+'''
+idade = input("Qual a sua idade?")
+idade = int(idade)
+
+if idade>= 18:
+    print("")
+else:
+    print("Você é menor de idade")'''
+
+#Ex5
+'''
+Anoatual = 2026
+idade = input("Digite sua idade ao lado")
+'''
+
+
+
+#Ex6
+'''
+nota = input("insira sua nota")
+nota = int(nota)
+
+if nota >= 7:
+    print("Aproved!")
+elif nota >=5:
+    print("Recuperação")
+else: 
+    print("Reprovado")'''
+
+#Ex7
+'''
+Temperatura = input("Qual a temperatura ambiente hoje?")
+Temperatura = int(Temperatura)
+
+if Temperatura >= 30:
+    print("Está quente")
+elif Temperatura <= 15:
+    print("Está Frio") 
+else:
+    print("Temperatura Agradável")'''
+
+for i in range(1,6):
+    print("Número", i)
